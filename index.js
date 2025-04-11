fbff66bf5e268ec716ecc8c43166e536ac0761eb
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-10 22:34:15 -0300

    js file to handle the creation of divs dynamically

---
+++ index.js
@@ -0,0 +1,9 @@
+import createDivs from "./modules/project.js";
+
+createDivs('Project Name', 'https://www.linkedin.com/feed/', 'https://www.linkedin.com/feed/');
+createDivs('Project Name', 'https://www.linkedin.com/feed/', 'https://www.linkedin.com/feed/');
+createDivs('Project Name', 'https://www.linkedin.com/feed/', 'https://www.linkedin.com/feed/');
+createDivs('Project Name', 'https://www.linkedin.com/feed/', 'https://www.linkedin.com/feed/');
+createDivs('Project Name', 'https://www.linkedin.com/feed/', 'https://www.linkedin.com/feed/');
+createDivs('Project Name', 'https://www.linkedin.com/feed/', 'https://www.linkedin.com/feed/');
+
